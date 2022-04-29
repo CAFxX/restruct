bdc7520e02310b148ad8d0eeb809df3491da3c1a
Carlo Alberto Ferraris <cafxx@strayorange.com>
2022-04-29 23:25:43 +0000

    more tests

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # restruct
-Parse regular expressions to structs
+Parse regular expressions to structs.
+
+## Supported conversions
+
+Matches can be parsed into the following field types:
+
+- `string`
+- `[]byte`
+- `[N]byte`
+- `int`/`uint`/`intN`/`uintN`/`uintptr`
+- `floatN`
+- `complexN`
+- `bool`
+- any type that implements [`encoding.TextUnmarshaler`](https://pkg.go.dev/encoding#TextUnmarshaler)
 
 ## Example
 
@@ -13,5 +26,5 @@ parse, _ := Compile[Foo](`(?<Bar>[a-z]+) (?<Baz>[0-9.]+)`)
 
 foo, _ := parse(`yadda 3.14`)
 
-fmt.Printf("%s/%f", foo.Bar, foo.Baz) // Output: yadda/3.14
+fmt.Printf("%s/%.3f", foo.Bar, foo.Baz) // Output: yadda/3.140
 ```
--- restruct_test.go
+++ restruct_test.go
@@ -12,10 +12,9 @@ func Example() {
 		Position
 		Foo int
 		Bar time.Time
-		Baz []byte
-	}](`(?P<Foo>[0-9]+)/(?P<Bar>[^\s]+)/(?P<Baz>x+)`)
+	}](`(?P<Foo>[0-9]+)/(?P<Bar>[^\s]+)`)
 
-	foo, err := parse("---> 42/2022-02-22T09:00:00Z/xxx <---")
+	foo, err := parse("---> 42/2022-02-22T09:00:00Z <---")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -23,15 +22,13 @@ func Example() {
 
 	fmt.Println(foo.Foo)
 	fmt.Println(foo.Bar)
-	fmt.Println(string(foo.Baz))
 	fmt.Println(foo)
 	fmt.Println(foo.Position)
 
 	// Output:
 	// 42
 	// 2022-02-22 09:00:00 +0000 UTC
-	// xxx
-	// 42/2022-02-22T09:00:00Z/xxx
+	// 42/2022-02-22T09:00:00Z
 	// 5
 }
 
@@ -51,6 +48,18 @@ func TestArray(t *testing.T) {
 	}](`(?P<X>[0-9]+)`)(`> 012345678 <`)
 	if err == nil {
 		t.Fatal("nil error")
+	}
+}
+
+func TestSlice(t *testing.T) {
+	r, err := Compile[struct {
+		X []byte
+	}](`(?P<X>[0-9]+)`)(`> 0123 <`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(r.X) != "0123" {
+		t.Fatal(r.X)
 	}
 }
 
@@ -130,5 +139,38 @@ func TestString(t *testing.T) {
 	}
 	if r.X != "0123" {
 		t.Fatal(r.X)
+	}
+}
+
+func TestTime(t *testing.T) {
+	r, err := Compile[struct {
+		X time.Time
+	}](`(?P<X>[0-9TZ:.+-]+)`)(`> 2022-02-22T09:00:00.123+01:00 <`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	exp, err := time.Parse(time.RFC3339Nano, `2022-02-22T09:00:00.123+01:00`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !r.X.Equal(exp) {
+		t.Fatal(r.X)
+	}
+
+	r, err = Compile[struct {
+		X time.Time
+	}](`(?P<X>[0-9]+)`)(`> 012345678 <`)
+	if err == nil {
+		t.Fatal("nil error")
+	}
+}
+
+func TestNoMatch(t *testing.T) {
+	_, err := Compile[struct {
+		X string
+	}](`(?P<X>[0-9]+)`)(`> xxx <`)
+	exp := errNoMatch{}
+	if err != exp {
+		t.Fatal(err)
 	}
 }
