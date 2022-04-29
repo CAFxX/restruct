66784da3aad1cd4e79121a08f210c8126ed0c1c3
Carlo Alberto Ferraris <cafxx@strayorange.com>
2022-04-29 14:10:06 +0000

    support byte arrays/slices

--- restruct_test.go
+++ restruct_test.go
@@ -2,6 +2,7 @@ package restruct
 
 import (
 	"fmt"
+	"testing"
 	"time"
 )
 
@@ -11,9 +12,10 @@ func Example() {
 		Position
 		Foo int
 		Bar time.Time
-	}](`(?P<Foo>[0-9]+)/(?P<Bar>[^\s]+)`)
+		Baz []byte
+	}](`(?P<Foo>[0-9]+)/(?P<Bar>[^\s]+)/(?P<Baz>x+)`)
 
-	foo, err := parse("---> 42/2022-02-22T09:00:00Z <---")
+	foo, err := parse("---> 42/2022-02-22T09:00:00Z/xxx <---")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -21,12 +23,33 @@ func Example() {
 
 	fmt.Println(foo.Foo)
 	fmt.Println(foo.Bar)
+	fmt.Println(string(foo.Baz))
 	fmt.Println(foo)
 	fmt.Println(foo.Position)
 
 	// Output:
 	// 42
 	// 2022-02-22 09:00:00 +0000 UTC
-	// 42/2022-02-22T09:00:00Z
+	// xxx
+	// 42/2022-02-22T09:00:00Z/xxx
 	// 5
+}
+
+func TestArray(t *testing.T) {
+	r, err := Compile[struct {
+		X [8]byte
+	}](`(?P<X>[0-9]+)`)(`> 0123 <`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.X != [8]byte{'0', '1', '2', '3', 0, 0, 0, 0} {
+		t.Fatal(r.X)
+	}
+
+	r, err = Compile[struct {
+		X [8]byte
+	}](`(?P<X>[0-9]+)`)(`> 012345678 <`)
+	if err == nil {
+		t.Fatal("nil error")
+	}
 }
